4d5ca0643d5776e333afcb9bb0ed3c64dbfb5720
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2021-03-19 21:43:55 +0300

    Update Fourier Transformlu Görüntü Frekans Bölgesi Gösterimi.m

--- Fourier Transformlu Görüntü Frekans Bölgesi Gösterimi.m
+++ Fourier Transformlu Görüntü Frekans Bölgesi Gösterimi.m
@@ -1,57 +1,71 @@
-%% Günseli ÇOLAK / 18253069
+%% GÃ¼nseli Ã‡OLAK / 18253069
 
-%% a)Resim 2 boyutlu Fourier transformu alarak bu görüntüye ait frekans bölgesi gösterimi
+%% a)Resim 2 boyutlu Fourier transformu alarak bu gÃ¶rÃ¼ntÃ¼ye ait frekans bÃ¶lgesi gÃ¶sterimi
 
 clear all,clc,close all
-IOrj = imread('peppers.png'); % Görüntüyü okudum.
-IOrj = rgb2gray(IOrj); % Görüntüyü gri seviyeye çevirir.
-f = double(IOrj); % Üzerinde çalışmak için double forma dönüştürdüm
-f1 = fft2(f); % 2 boyutlu DFT yapar, çıkışı matristir.
-f2 = fftshift(f1); % Bir tranformasyonu kaydırır. 
+IOrj = imread('peppers.png'); % GÃ¶rÃ¼ntÃ¼yÃ¼ okudum.
+IOrj = rgb2gray(IOrj); % GÃ¶rÃ¼ntÃ¼yÃ¼ gri seviyeye Ã§evirir.
+f = double(IOrj); % Ãœzerinde Ã§alÄ±ÅŸmak iÃ§in double forma dÃ¶nÃ¼ÅŸtÃ¼rdÃ¼m
+f1 = fft2(f); % 2 boyutlu DFT yapar, Ã§Ä±kÄ±ÅŸÄ± matristir.
+f2 = fftshift(f1); % Bir tranformasyonu kaydÄ±rÄ±r. 
 
 %% plot
-subplot(3,3,1), imshow(uint8(f)); title('Görüntünün Gri Hali'); %% görüntünün gri hali
-subplot(3,3,2), imshow(log(abs(f1)),[]); title('Fourier Transformlu Görüntü');
-subplot(3,3,3), imshow(log(abs(f2)),[]); title (' Merkezde Toplanmış Hali');
-subplot(3,3,5), mesh(abs(f1)); title('Fourier Transformlu Frekans Bölgesi');
-subplot(3,3,6), mesh(abs(f2)); title('Merkezde Toplanmış Frekans Bölgesi');
+subplot(3,3,1), imshow(uint8(f)); title('GÃ¶rÃ¼ntÃ¼nÃ¼n Gri Hali'); %% gÃ¶rÃ¼ntÃ¼nÃ¼n gri hali
+subplot(3,3,2), imshow(log(abs(f1)),[]); title('Fourier Transformlu GÃ¶rÃ¼ntÃ¼');
+subplot(3,3,3), imshow(log(abs(f2)),[]); title (' Merkezde ToplanmÄ±ÅŸ Hali');
+subplot(3,3,5), mesh(abs(f1)); title('Fourier Transformlu Frekans BÃ¶lgesi');
+subplot(3,3,6), mesh(abs(f2)); title('Merkezde ToplanmÄ±ÅŸ Frekans BÃ¶lgesi');
 
-%% b)Resim üzerine harici olarak 
-%% 10dB beyaz gürültü ekleyerek a seçeneğinde belirtilen gösterimi
+%% b)Resim Ã¼zerine harici olarak 
+%% 10dB beyaz gÃ¼rÃ¼ltÃ¼ ekleyerek a seÃ§eneÄŸinde belirtilen gÃ¶sterimi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 %% AGS
 clear all,clc,close all
-IOrj = imread('peppers.png'); % Görüntüyü okudum
+IOrj = imread('peppers.png'); % GÃ¶rÃ¼ntÃ¼yÃ¼ okudum
 IOrj=rgb2gray(IOrj);
-I = double(IOrj); % üzerinde çalışmak için double forma dönüştürdüm
+I = double(IOrj); % Ã¼zerinde Ã§alÄ±ÅŸmak iÃ§in double forma dÃ¶nÃ¼ÅŸtÃ¼rdÃ¼m
 [N M] = size(I);
 %SNR = input ('dB olarak deger girin: ');
 SNR = 10;
 PS = sum(sum(I.^2))/(N*M); %power of signal
 sigma = PS./(10^(0.1*SNR));
-W = sqrt(sigma).*randn(N,M); %PN %% N M lik gürültü haline dönüştürdüm. %% Gürültüyü oluştuduk.
-A = plus(I,W); % görüntünün üzerine ekledik artık A bozulmuş görüntü oldu.
+W = sqrt(sigma).*randn(N,M); %PN %% N M lik gÃ¼rÃ¼ltÃ¼ haline dÃ¶nÃ¼ÅŸtÃ¼rdÃ¼m. %% GÃ¼rÃ¼ltÃ¼yÃ¼ oluÅŸtuduk.
+A = plus(I,W); % gÃ¶rÃ¼ntÃ¼nÃ¼n Ã¼zerine ekledik artÄ±k A bozulmuÅŸ gÃ¶rÃ¼ntÃ¼ oldu.
 
 %% Fourier Transformu
 A1=fft2(A); 
 A2=fftshift(A1);
 
 %% plot 
-subplot(3,3,1), imshow(IOrj); title('Görüntünün Gri Hali'); 
-subplot(3,3,2), imshow(uint8(A)); title('Gürültü Eklenmiş Hali'); % 10 db beyaz gürültü eklenmiş görüntü
-subplot(3,3,3), imshow(log(abs(A1)),[]); title('Fourier Transformlu Görüntü');
-subplot(3,3,4), imshow(log(abs(A2)),[]); title('Merkezde Toplanmış Hali'); 
-subplot(3,3,5), mesh(abs(A1)); title('Fourier Transformlu Frekans Bölgesi');
-subplot(3,3,6), mesh(abs(A2)); title('Merkezde Toplanmış Frekans Bölgesi');
+subplot(3,3,1), imshow(IOrj); title('GÃ¶rÃ¼ntÃ¼nÃ¼n Gri Hali'); 
+subplot(3,3,2), imshow(uint8(A)); title('GÃ¼rÃ¼ltÃ¼ EklenmiÅŸ Hali'); % 10 db beyaz gÃ¼rÃ¼ltÃ¼ eklenmiÅŸ gÃ¶rÃ¼ntÃ¼
+subplot(3,3,3), imshow(log(abs(A1)),[]); title('Fourier Transformlu GÃ¶rÃ¼ntÃ¼');
+subplot(3,3,4), imshow(log(abs(A2)),[]); title('Merkezde ToplanmÄ±ÅŸ Hali'); 
+subplot(3,3,5), mesh(abs(A1)); title('Fourier Transformlu Frekans BÃ¶lgesi');
+subplot(3,3,6), mesh(abs(A2)); title('Merkezde ToplanmÄ±ÅŸ Frekans BÃ¶lgesi');
  
-%% c)Aradaki farkı yorumlayınız.
+%% c)Aradaki farkÄ± yorumlayÄ±nÄ±z.
 
-%% Orjinal resim için 2 boyutlu Fourier Transformu alarak(a) ve resim
-%% üzerine 10 dB beyaz gürültü ekleyip Fourier Transformu alarak(b) elde 
-%% ettiğim görüntüler birbirinden farklı olmasına rağmen frekans bölgesi
-%% gösterimi birbirinin aynıdır.
-% a'da daha net ve canlı, beyaz nokta parlak görüntü elde ettim. 
-% b'de daha bulanık ve soluk, beyaz nokta soluk görüntü elde ettim.
-% A ve B şıkkında da fourier transformlu frekans bölgesi gösterimi genlikleri 
-% küçük, merkezde toplanmış frekans bölgesi gösteriminde ise genlikler yüksek 
-% değerlere sahiptir.
+%% Orjinal resim iÃ§in 2 boyutlu Fourier Transformu alarak(a) ve resim
+%% Ã¼zerine 10 dB beyaz gÃ¼rÃ¼ltÃ¼ ekleyip Fourier Transformu alarak(b) elde 
+%% ettiÄŸim gÃ¶rÃ¼ntÃ¼ler birbirinden farklÄ± olmasÄ±na raÄŸmen frekans bÃ¶lgesi
+%% gÃ¶sterimi birbirinin aynÄ±dÄ±r.
+% a'da daha net ve canlÄ±, beyaz nokta parlak gÃ¶rÃ¼ntÃ¼ elde ettim. 
+% b'de daha bulanÄ±k ve soluk, beyaz nokta soluk gÃ¶rÃ¼ntÃ¼ elde ettim.
+% A ve B ÅŸÄ±kkÄ±nda da fourier transformlu frekans bÃ¶lgesi gÃ¶sterimi genlikleri 
+% kÃ¼Ã§Ã¼k, merkezde toplanmÄ±ÅŸ frekans bÃ¶lgesi gÃ¶steriminde ise genlikler yÃ¼ksek 
+% deÄŸerlere sahiptir.
